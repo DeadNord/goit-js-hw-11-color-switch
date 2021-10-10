2685b17e507897c793a388cd6d7175c38e796155
DeadNord <eosipopo@gmail.com>
2021-10-10 19:50:15 +0300

    change-color

--- js/color-switcher.js
+++ js/color-switcher.js
@@ -1,15 +1,38 @@
-const colors = [
-  '#FFFFFF',
-  '#2196F3',
-  '#4CAF50',
-  '#FF9800',
-  '#009688',
-  '#795548',
-];
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
+// Links
+const refs = {
+  bodyColor: document.querySelector('body'),
+  startButton: document.querySelector('button[data-action="start"'),
+  stopButton: document.querySelector('button[data-action="stop"'),
+};
 
+// Listeners
+refs.startButton.addEventListener('click', start);
+refs.stopButton.addEventListener('click', stop);
 
-
+// Randimise
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+// ChangeColor
+function changeBackground(color) {
+  refs.bodyColor.style.backgroundColor = color;
+}
+
+let intervalColorChange = undefined;
+function start() {
+  intervalColorChange = setInterval(randomNumber => {
+    randomNumber = randomIntegerFromInterval(0, 5);
+    changeBackground(colors[randomNumber]);
+  }, 1000);
+  refs.stopButton.removeAttribute('disabled');
+  refs.startButton.setAttribute('disabled', true);
+}
+
+function stop() {
+  clearInterval(intervalColorChange);
+  refs.startButton.removeAttribute('disabled');
+  refs.stopButton.setAttribute('disabled', true);
+}
